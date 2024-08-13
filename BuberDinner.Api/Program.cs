848dd2f3312289d97603d7b94d14a4d3278baba4
Vijay kumar <vijay00200@live.com>
2024-08-13 20:13:55 +0530

    Clean code -DDD intial commit

---
+++ BuberDinner.Api/Program.cs
@@ -0,0 +1,19 @@
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    // Add services to the container.
+    builder.Services.AddApplication()
+                    .AddInfrastructure(builder.Configuration);
+    builder.Services.AddControllers();
+}
+
+
+var app = builder.Build();
+{
+    app.UseHttpsRedirection();
+    app.MapControllers();
+
+    app.Run();
+}
